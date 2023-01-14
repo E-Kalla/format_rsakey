5a8f644b7f40c00ee4e147b4b5be956b967d53d6
E-Kalla <elias.kalla-diouloufet@epitech.eu>
2023-01-14 15:04:25 +0100

    add script

---
+++ convert_private_key.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 key_file"
+    exit 1
+fi
+
+key_file=$1
+new_key="new-key"
+format_key="format-key"
+
+puttygen $key_file -O private-sshcom -o $new_key
+ssh-keygen -i -f $new_key > $format_key
+
